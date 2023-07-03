18dd81aa9766822d6fd2418d6f56fdcb574ca907
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-07-03 11:12:28 +0530

    Added : Refactor the Code to write a Class Method to Compute Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,46 +1,49 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
-    static final int IS_FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
+    static final int Wage_Per_Hour = 20;
+    static final int Full_Time_Hour = 8;
+    static final int Part_Time_Hour = 4;
+    static final int Is_Part_Time_Hour = 1;
+    static final int Is_Full_Time_Hour = 2;
+    static final int Working_Days_Per_Month = 20;
 
-    static final  int WORKING_HOURS_PER_MONTH = 100;
-    static final int WORKING_DAYS_PER_MONTH = 20;
+    static int employeeAttendance() {
+        int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
+        return employeeAttendance;
+    }
 
-    public static void main(String[] args) {
-        System.out.println("welcome to Employee wage program");
+    static int switchCase() {
+        int day = 1;
         int totalWage = 0;
-        int dayCount = 1;
-        int workingHours = 0;
-
-        while (dayCount <=WORKING_DAYS_PER_MONTH &&  workingHours < WORKING_HOURS_PER_MONTH  ) {
-            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(employeeCheck);
+        while (day <= Working_Days_Per_Month) {
             int dailyWage = 0;
-
-            switch (employeeCheck) {
-                case IS_PART_TIME:
-                    dailyWage = PART_TIME_HOUR * WAGE_PER_HOUR;
-                    workingHours += PART_TIME_HOUR;
-                    System.out.println("Employee is part time" + dailyWage);
+            int employeeAttendanceCheck = employeeAttendance();
+            System.out.println(employeeAttendanceCheck);
+            switch (employeeAttendanceCheck) {
+                case Is_Full_Time_Hour:
+                    System.out.println("Employee is Present");
+                    dailyWage = Wage_Per_Hour * Full_Time_Hour;
                     break;
-                case IS_FULL_TIME:
-                    dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-                    workingHours +=FULL_DAY_HOUR;
-                    System.out.println("Employee is full time " + dailyWage);
+                case Is_Part_Time_Hour:
+                    dailyWage = Wage_Per_Hour * Part_Time_Hour;
+                    System.out.println("Employee is Part Time");
                     break;
                 default:
-                    System.out.println("Employee is absent");
-                    break;
+                    System.out.println("Absent");
             }
-            dayCount++;
             totalWage = totalWage + dailyWage;
+            System.out.println("daily Wage ==> " + day + " day => " + dailyWage);
+            day++;
         }
-        System.out.println("Total hours "+ workingHours);
-        System.out.println("daily wage for month "+ totalWage);
-
+        System.out.println("Total Wage => " + totalWage);
+        return totalWage;
     }
+
+
+    public static void main(String[] args) {
+        int totalWage=switchCase();
+        System.out.println(totalWage);
+    }
+
 }
